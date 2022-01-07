d44bc1ffdaff6c6e11590fbf5786e7066a0726f7
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-07 17:25:52 -0300

    Create styles.js

---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  formContainer: {
+    flexDirection: 'row',
+    padding: 20,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#22223B',
+  },
+  textInnput: {
+    flex: 0.8,
+    marginHorizontal: 20,
+    borderColor: '#F2E9E4',
+    color: '#F2E9E4',
+    borderRadius: 10,
+    backgroundColor: '#9A8C98',
+  },
+});
+
+export default styles;
