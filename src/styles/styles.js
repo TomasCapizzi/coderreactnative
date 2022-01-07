d46b5cd8c623fe8226128f19de84c5925b49e51b
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-07 17:26:14 -0300

    App Styles

---
+++ src/styles/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 0,
+    padding: 0,
+    backgroundColor: '#4A4E69',
+  },
+});
+
+export default styles;
